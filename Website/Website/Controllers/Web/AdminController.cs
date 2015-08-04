a0ab6bd4a9e27d9b73065e209644764cef20a99f
Simon Gellis <simongellis@gmail.com>
2015-08-03 23:34:26 -0400

    Adds a ResetPassword admin option, prettifies admin page
    We should really get rid of this gratuitous SHA256ing and just get an SSL cert...

--- Website/Website/Controllers/Web/AdminController.cs
+++ Website/Website/Controllers/Web/AdminController.cs
@@ -51,6 +51,13 @@ namespace Website.Controllers.Web
         }
 
         [Authorize(Roles = "Administrator")]
+        [Route("ResetPassword")]
+        public ActionResult ResetPassword()
+        {
+            return View();
+        }
+
+        [Authorize(Roles = "Administrator")]
         [HttpPost, Route("AddRole")]
         public async Task<ActionResult> AddRole(AddRoleViewModel vm)
         {
@@ -63,19 +70,39 @@ namespace Website.Controllers.Web
                     return View();
                 }
                 var result = await UserManager.AddClaimAsync(user.Id, new Claim(ClaimTypes.Role, vm.Role));
-                if (result.Succeeded)
-                {
-                    return View("Index");
-                }
-                else
-                {
-                    foreach (var error in result.Errors)
-                    {
-                        ModelState.AddModelError("", error);
-                    }
-                }
+                if (WasSuccessful(result)) return View("Index");
             }
             return View();
+        }
+
+        [Authorize(Roles = "Administrator")]
+        [HttpPost, Route("ResetPassword")]
+        public async Task<ActionResult> ResetPassword(ResetPasswordViewModel vm)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = UserManager.FindByName(vm.Username);
+                if (user == null)
+                {
+                    ModelState.AddModelError("", "User not found");
+                    return View();
+                }
+                var reset = await UserManager.RemovePasswordAsync(user.Id);
+                if (!WasSuccessful(reset)) return View();
+                var add = await UserManager.AddPasswordAsync(user.Id, vm.Password);
+                if (WasSuccessful(add)) return View("Index");
+            }
+            return View();
+        }
+
+        private bool WasSuccessful(IdentityResult result)
+        {
+            if (result.Succeeded) return true;
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+            return false;
         }
     }
 }
