50473526baf8599dff61c4c74b31ba26a47e5a21
Simon Gellis <simongellis@gmail.com>
2015-10-12 00:52:18 -0400

    Adds a dummy migration to deal with merge conflicts

---
+++ Website/Website/Migrations/201510120449112_Dummy1.cs
@@ -0,0 +1,18 @@
+namespace Website.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Dummy1 : DbMigration
+    {
+        public override void Up()
+        {
+            //http://stackoverflow.com/a/19158039
+        }
+
+        public override void Down()
+        {
+            //http://stackoverflow.com/a/19158039
+        }
+    }
+}
